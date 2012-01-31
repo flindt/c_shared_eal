629c661ad8674843c2d56d272b03583e0656dc2e
Lasse <lars_andre@hotmail.com>
2012-01-31 10:45:48 +0100

    Hello World Start of PSoC 3 project

---
+++ PSoC_3/Group_3_PSoC_Project_3/src/Group_3_PSoC_Project_3.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Group_3_PSoC_Project_3.c
+ Author      : Lars
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("State it"); /* prints State it */
+	return EXIT_SUCCESS;
+}
