1d01d9b2fa55eee24f0e4b0771a65461414c8469
pfl <pfl@eal.dk>
2012-02-01 09:51:01 +0100

    added group 3 project

---
+++ PSoC_3/Group_3_PSoC_Project_3/src/Group_3_PSoC_Project_3.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Group_3_PSoC_Project_3.c
+ Author      : Lars, Ibrahim, Lukas
+ Version     :
+ Copyright   : Your copyright notice
+ Description : PSoC 4 butten interface, Project 3, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("State it"); /* prints State it */
+	return EXIT_SUCCESS;
+}
